7fc21b053e4ecf25d96fe5e3a958a8b5dac73415
Dikshant <dikkpsd@gmail.com>
2024-06-11 22:37:41 +0530

    Added some login frontend

---
+++ frontend-judge/src/routes/Header.svelte
@@ -0,0 +1,117 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import user from '$lib/images/user.svg';
+</script>
+
+<header>
+	<div class="corner">
+		<a href="https://kit.svelte.dev">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+
+	<nav>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+
+			<li aria-current={$page.url.pathname === '/hello' ? 'page' : undefined}>
+				<a href="/">Hello</a>
+			</li>
+			
+		</ul>
+	</nav>
+
+	<div class="corner">
+		<a href="/login">
+			<img src={user} alt="User" />
+		</a>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		background-color:var(--color-theme-1);
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 1.5em;
+		height: 1.5em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 2em;
+		display: flex;
+		border-radius: 20px;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--color-theme-3);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top:var(--size) solid var(--color-theme-2);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>
