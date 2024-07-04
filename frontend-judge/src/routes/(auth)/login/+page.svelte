c88cbceb0f04f669e5c16876072255e44d7892eb
Dikshant <dikkpsd@gmail.com>
2024-07-05 00:16:51 +0530

    Added login page

---
+++ frontend-judge/src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+    import "$lib/auth_styles.css";
+
+    let email: string;
+    let password: string;
+
+
+ </script> 
+<form class = "input-form" method = "POST">
+    <h1 id="tex"> Login to HELL</h1>
+    <input type="text" name="email" bind:value={email} class= "inputprops" placeholder="email"><br>
+    <input type="password" name="password" bind:value={password} class= "inputprops" placeholder="password"><br>
+    <input type="submit"  value="Login" class="buttonprops"><br>
+    <input type="button" value="Register Instead" class="buttonprops">
+</form>
+
